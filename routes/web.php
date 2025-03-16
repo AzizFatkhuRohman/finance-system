5bf997a36a62dc81e140e1c075fd8c972884ef83
AzizFatkhuRohman <azizfatkhurohman@outlook.com>
2025-03-16 19:51:19 +0700

    laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
 
-Route::post('dashboard', function () {
-    return view('admin.dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Customer
-Route::get('customer', function () {
-    return view('admin.data_customer');
-});
-
-//Suplier
-Route::get('suplier', function () {
-    return view('admin.data_suplier');
-});
-
-//Produk
-Route::get('produk', function () {
-    return view('admin.data_produk');
-});
-
-//Penjualan
-Route::get('penjualan', function () {
-    return view('admin.penjualan');
-});
-
-//Biaya
-Route::get('biaya', function () {
-    return view('admin.biaya');
-});
+require __DIR__.'/auth.php';
