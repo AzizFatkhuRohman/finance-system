834da893a1c2a823605e2c9e8dd69bb9eee354ec
AzizFatkhuRohman <azizfatkhurohman@outlook.com>
2025-03-16 20:17:16 +0700

    Dashboard & Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,90 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Login</title>
+    <meta name="description" content="A responsive bootstrap 4 admin dashboard template by hencework" />
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Toggles CSS -->
+    <link href="{{ asset('vendors/jquery-toggles/css/toggles.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('vendors/jquery-toggles/css/themes/toggles-light.css') }}" rel="stylesheet" type="text/css">
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('dist/css/style.css') }}" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader-it">
+        <div class="loader-pendulums"></div>
+    </div>
+    <!-- /Preloader -->
+
+    <!-- HK Wrapper -->
+    <div class="hk-wrapper">
+
+        <!-- Main Content -->
+        <div class="hk-pg-wrapper hk-auth-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-12 pa-0">
+                        <div class="auth-form-wrap pt-xl-0 pt-70">
+                            <div class="auth-form w-xl-30 w-lg-55 w-sm-75 w-100">
+                                <a class="auth-brand text-center d-block mb-20" href="#">
+                                    <img class="brand-img" src="{{ asset('dist/img/logo_dwi.png') }}" alt="brand" />
+                                </a>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <h1 class="display-4 text-center mb-10">Welcome</h1>
+                                    <p class="text-center mb-30">Sign in to your account</p>
+                                    <div class="form-group">
+                                        <input class="form-control" placeholder="Email" type="email" name="email"
+                                            required>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <input class="form-control" placeholder="Password" type="password"
+                                                name="password" required>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text"><span class="feather-icon"><i
+                                                            data-feather="eye-off"></i></span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="custom-control custom-checkbox mb-25">
+                                        <input class="custom-control-input" id="same-address" type="checkbox" checked>
+                                        <label class="custom-control-label font-14" for="same-address">Keep me logged
+                                            in</label>
+                                    </div>
+                                    <button class="btn btn-primary btn-block" type="submit">Login</button>
+                                    <div class="option-sep">Success</div>
+                                    <p class="text-center">Tidak bisa Login ? <a
+                                            href="{{ url('forgot-password') }}">Forgot password</a></p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!-- /Main Content -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    </div>
+    <!-- /HK Wrapper -->
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <!-- JavaScript -->
+    <script src="{{ asset('vendors/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendors/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('dist/js/jquery.slimscroll.js') }}"></script>
+    <script src="{{ asset('dist/js/dropdown-bootstrap-extended.js') }}"></script>
+    <script src="{{ asset('dist/js/feather.min.js') }}"></script>
+    <script src="{{ asset('dist/js/init.js') }}"></script>
+</body>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,87 +1,102 @@
 <nav class="navbar navbar-expand-xl navbar-light fixed-top hk-navbar hk-navbar-alt">
-            <a class="navbar-toggle-btn nav-link-hover navbar-toggler" href="javascript:void(0);" data-toggle="collapse" data-target="#navbarCollapseAlt" aria-controls="navbarCollapseAlt" aria-expanded="false" aria-label="Toggle navigation"><span class="feather-icon"><i data-feather="menu"></i></span></a>
-            <a class="navbar-brand" href="#">
-                <img class="brand-img d-inline-block align-top" src="{{ asset('dist/img/logo_dwi2.png') }}" alt="brand" />
-            </a>
-            <div class="collapse navbar-collapse" id="navbarCollapseAlt">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown show-on-hover active">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Dashboard
-							</a>                        
-                    </li>
-                    <li class="nav-item dropdown show-on-hover">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Master Data
-							</a>
-                        <div class="dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">                            
-                            <a class="dropdown-item" href="{{url('customer')}}">Customer</a>
-                            <a class="dropdown-item" href="{{ url('suplier') }}">Suplier</a>
-                            <a class="dropdown-item" href="{{ url('produk') }}">Product</a>                            
+    <a class="navbar-toggle-btn nav-link-hover navbar-toggler" href="javascript:void(0);" data-toggle="collapse"
+        data-target="#navbarCollapseAlt" aria-controls="navbarCollapseAlt" aria-expanded="false"
+        aria-label="Toggle navigation"><span class="feather-icon"><i data-feather="menu"></i></span></a>
+    <a class="navbar-brand" href="#">
+        <img class="brand-img d-inline-block align-top" src="{{ asset('dist/img/logo_dwi2.png') }}" alt="brand" />
+    </a>
+    <div class="collapse navbar-collapse" id="navbarCollapseAlt">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown show-on-hover active">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item dropdown show-on-hover">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Master Data
+                </a>
+                <div class="dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                    <a class="dropdown-item" href="{{ url('customer') }}">Customer</a>
+                    <a class="dropdown-item" href="{{ url('suplier') }}">Suplier</a>
+                    <a class="dropdown-item" href="{{ url('produk') }}">Product</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('penjualan') }}">Penjualan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('biaya') }}">Biaya</a>
+            </li>
+            <li class="nav-item dropdown show-on-hover">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Laporan
+                </a>
+                <div class="dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                    <a class="dropdown-item" href="jurnal.html">Jurnal Umum</a>
+                    <a class="dropdown-item" href="neraca.html">Neraca</a>
+                    <a class="dropdown-item" href="buku_besar.html">Buku Besar</a>
+                    <a class="dropdown-item" href="rl.html">Rugi/Laba</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown show-on-hover">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Setting
+                </a>
+                <div class="dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                    <a class="dropdown-item" href="data_akun.html">Akun</a>
+                    <a class="dropdown-item" href="#">Akses</a>
+                </div>
+            </li>
+        </ul>
+        <form class="navbar-search-alt">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><span class="feather-icon"><i
+                                data-feather="search"></i></span></span>
+                </div>
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+            </div>
+        </form>
+    </div>
+    <ul class="navbar-nav hk-navbar-content">
+        <li class="nav-item dropdown dropdown-authentication">
+            <a class="nav-link dropdown-toggle no-caret" href="#" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <div class="media">
+                    <div class="media-img-wrap">
+                        <div class="avatar">
+                            <img src="{{ asset('dist/img/avatar5.jpg') }}" alt="user"
+                                class="avatar-img rounded-circle">
                         </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('penjualan') }}" >Penjualan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('biaya') }}" >Biaya</a>
-                    </li>
-                    <li class="nav-item dropdown show-on-hover">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Laporan
-                            </a>
-                        <div class="dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">                            
-                            <a class="dropdown-item" href="jurnal.html">Jurnal Umum</a>
-                            <a class="dropdown-item" href="neraca.html">Neraca</a>
-                            <a class="dropdown-item" href="buku_besar.html">Buku Besar</a>
-                            <a class="dropdown-item" href="rl.html">Rugi/Laba</a>                            
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown show-on-hover">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Setting
-                            </a>
-                        <div class="dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
-                            <a class="dropdown-item" href="data_akun.html">Akun</a>
-                            <a class="dropdown-item" href="#">Akses</a>
-                        </div>
-                    </li>                    
-                </ul>
-                <form class="navbar-search-alt">
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><span class="feather-icon"><i data-feather="search"></i></span></span>
-                        </div>
-                        <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                        <span class="badge badge-success badge-indicator"></span>
                     </div>
+                    <div class="media-body">
+                        <span>Admin<i class="zmdi zmdi-chevron-down"></i></span>
+                    </div>
+                </div>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" data-dropdown-in="flipInX" data-dropdown-out="flipOutX">
+
+                <div class="dropdown-divider"></div>
+                <div class="sub-dropdown-menu show-on-hover">
+                    <a href="#" class="dropdown-toggle dropdown-item no-caret"><i
+                            class="zmdi zmdi-check text-success"></i>Online</a>
+                    <a class="dropdown-item" href="#"><i
+                            class="dropdown-icon zmdi zmdi-settings"></i><span>Change Password</span></a>
+
+                </div>
+                <div class="dropdown-divider"></div>
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button class="dropdown-item" type="submit"><i
+                            class="dropdown-icon zmdi zmdi-power"></i><span>Log out</span></button>
                 </form>
             </div>
-            <ul class="navbar-nav hk-navbar-content">                
-                <li class="nav-item dropdown dropdown-authentication">
-                    <a class="nav-link dropdown-toggle no-caret" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div class="media">
-                            <div class="media-img-wrap">
-                                <div class="avatar">
-                                    <img src="{{ asset('dist/img/avatar5.jpg') }}" alt="user" class="avatar-img rounded-circle">
-                                </div>
-                                <span class="badge badge-success badge-indicator"></span>
-                            </div>
-                            <div class="media-body">
-                                <span>Admin<i class="zmdi zmdi-chevron-down"></i></span>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" data-dropdown-in="flipInX" data-dropdown-out="flipOutX">
-                                                
-                        <div class="dropdown-divider"></div>
-                        <div class="sub-dropdown-menu show-on-hover">
-                            <a href="#" class="dropdown-toggle dropdown-item no-caret"><i class="zmdi zmdi-check text-success"></i>Online</a>
-                            <a class="dropdown-item" href="#"><i class="dropdown-icon zmdi zmdi-settings"></i><span>Change Password</span></a>
-                            
-                        </div>
-                        <div class="dropdown-divider"></div>                        
-                        <a class="dropdown-item" href="#"><i class="dropdown-icon zmdi zmdi-power"></i><span>Log out</span></a>
-                    </div>
-                </li>
-            </ul>
-        </nav>
+        </li>
+    </ul>
+</nav>
