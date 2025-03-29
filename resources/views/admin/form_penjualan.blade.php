9aa79a5f1f55065465355d4020767c2371696531
anwarhlmn27 <anwarhlmn27@gmail.com>
2025-03-29 11:27:45 +0700

    form penjualan

--- resources/views/admin/form_penjualan.blade.php
+++ resources/views/admin/form_penjualan.blade.php
@@ -1,112 +1,142 @@
 @extends('partials.app')
 @section('content')
-        <nav class="hk-breadcrumb" aria-label="breadcrumb">
-            <ol class="breadcrumb breadcrumb-light bg-transparent">
-                <li class="breadcrumb-item"><a href="#">Forms</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Form Penjualan</li>
-            </ol>
-        </nav>
+    <nav class="hk-breadcrumb" aria-label="breadcrumb">
+        <ol class="breadcrumb breadcrumb-light bg-transparent">
+            <li class="breadcrumb-item"><a href="#">Forms</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Form Penjualan</li>
+        </ol>
+    </nav>
 
-        <div class="container-fluid">
-            <div class="hk-pg-header">
-                <h4 class="hk-pg-title"><span class="pg-title-icon"><span class="feather-icon"><i
-                                data-feather="align-left"></i></span></span>Form Penjualan</h4>
-            </div>
-
-            <div class="row">
-                <div class="col-xl-12">
-                    <section class="hk-sec-wrapper">
-                        <!-- <h5 class="hk-sec-title">Tambah Penjualan</h5>
-                        <p class="mb-25">Isi form berikut dengan lengkap.</p> -->
-                        <div class="row">
-                            <div class="col-sm">
-                                <form action="{{ url('penjualan') }}" method="post">
-                                    @csrf
-                                    
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <label for="nama_bank">Nama Customer</label>
-                                            <input class="form-control @error('nama_customer') is-invalid @enderror"
-                                                id="nama_bank" name="nama_bank" type="text"
-                                                value="{{ old('nama_customer') }}">
-                                            @error('nama_bank')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-                                        <div class="col-lg-6">
-                                            <label for="tanggal">Tanggal</label>
-                                            <input class="form-control @error('tgl') is-invalid @enderror" id="cabang"
-                                                name="cabang" type="date" value="{{ old('tgl') }}">
-                                            @error('tgl')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6 form-group">
-                                            <label for="nomor_rekening">Alamat Pengiriman</label>
-                                            <textarea name="alamat"
-                                                class="form-control @error('nomor_rekening') is-invalid @enderror"
-                                                value="{{ old('nomor_rekening') }}"></textarea>
-                                            @error('nomor_rekening')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-                                        <div class="col-md-6 form-group">
-                                            <label for="nama_pemilik">Kode Transaksi</label>
-                                            <input type="text" name="kode_transaksi"
-                                                class="form-control @error('kode_transaksi') is-invalid @enderror"
-                                                value="{{ old('kode_transaksi') }}">
-                                            @error('kode_transaksi')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror
-                                        </div>
-                                    </div>
-                                </br>
-                                    <div class="table-wrap">
-                                        <table class="table table-hover mb-x0">
-                                            <thead>
-                                                <tr>
-                                                    <th>Kode Akun</th>
-                                                    <th>Produk</th>
-                                                    <th>Quantity</th>
-                                                    <th>Harga</th>
-                                                    <th>Total Harga</th>
-                                                    
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                
-                                                    <tr>
-                                                        <td><input type="text" name="kode_transaksi"
-                                                class="form-control"
-                                                value=""></td>
-                                                        <td><input type="text" name="kode_transaksi"
-                                                class="form-control"
-                                                value=""></td>
-                                                        <td><input type="text" name="kode_transaksi"
-                                                class="form-control"
-                                                value=""></td>
-                                                        <td><input type="text" name="kode_transaksi"
-                                                class="form-control"
-                                                value=""></td>
-                                                        <td><input type="text" name="kode_transaksi"
-                                                class="form-control"
-                                                value=""></td>
-                                                        
-                                                    </tr>
-                                            
-                                            </tbody>
-                                    
-                                        </table>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                </form>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
+    <div class="container-fluid">
+        <div class="hk-pg-header">
+            <h4 class="hk-pg-title">
+                <span class="pg-title-icon">
+                    <span class="feather-icon"><i data-feather="align-left"></i></span>
+                </span>
+                Form Penjualan
+            </h4>
         </div>
 
+        <div class="row">
+            <div class="col-xl-12">
+                <section class="hk-sec-wrapper">
+                    <div class="row">
+                        <div class="col-sm">
+                            <form action="{{ url('penjualan') }}" method="post">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <label for="nama_bank">Nama Customer</label>
+                                        <input class="form-control form-control-sm @error('nama_customer') is-invalid @enderror"
+                                            id="nama_bank" name="nama_bank" type="text"
+                                            value="{{ old('nama_customer') }}">
+                                        @error('nama_bank')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label for="tanggal">Tanggal</label>
+                                        <input class="form-control form-control-sm @error('tgl') is-invalid @enderror" id="cabang"
+                                            name="cabang" type="date" value="{{ old('tgl') }}">
+                                        @error('tgl')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 form-group">
+                                        <label for="nomor_rekening">Alamat Pengiriman</label>
+                                        <textarea name="alamat"
+                                            class="form-control @error('nomor_rekening') is-invalid @enderror">{{ old('nomor_rekening') }}</textarea>
+                                        @error('nomor_rekening')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                    <div class="col-md-6 form-group">
+                                        <label for="nama_pemilik">Kode Transaksi</label>
+                                        <input type="text" name="kode_transaksi"
+                                            class="form-control form-control-sm @error('kode_transaksi') is-invalid @enderror"
+                                            value="{{ old('kode_transaksi') }}">
+                                        @error('kode_transaksi')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <br>
+                                <div class="table-wrap">
+                                    <table class="table table-hover mb-x0">
+                                        <thead>
+                                            <tr>
+                                                <th>Kode Akun</th>
+                                                <th>Produk</th>
+                                                <th>Quantity</th>
+                                                <th>Harga</th>
+                                                <th>Total Harga</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <select class="form-control custom-select-sm">
+                                                        <option selected>Kode Akun</option>
+                                                        <option value="1">One</option>
+                                                        <option value="2">Two</option>
+                                                        <option value="3">Three</option>
+                                                    </select>
+                                                </td>
+                                                <td>
+                                                    <select class="form-control custom-select-sm">
+                                                        <option selected>Pilih Produk</option>
+                                                        <option value="1">One</option>
+                                                        <option value="2">Two</option>
+                                                        <option value="3">Three</option>
+                                                    </select>
+                                                </td>
+                                                <td><input type="number" name="quantity" class="form-control form-control-sm" value=""></td>
+                                                <td><input type="text" name="harga" class="form-control form-control-sm" value=""></td>
+                                                <td><input type="text" name="total_harga" class="form-control form-control-sm" value="" readonly></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </br>
+                                </div>
+                                <div class="table-wrap">
+                                    <table class="table table-hover mb-x0">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 20%;"></th>
+                                                <th style="width: 20%;"></th>
+                                                <th style="width: 20%;"></th>
+                                                <th>Subtotal</th>
+                                                <th><input type="text" class="form-control form-control-sm"></th>
+                                            </tr>
+                                            <tr>
+                                                <th style="width: 20%;"></th>
+                                                <th style="width: 20%;"></th>
+                                                <th style="width: 20%;"></th>
+                                                <th>Diskon</th>
+                                                <th><input type="text" class="form-control form-control-sm"></th>
+                                            </tr>
+                                            <tr>
+                                                <th style="width: 20%;"></th>
+                                                <th style="width: 20%;"></th>
+                                                <th style="width: 20%;"></th>
+                                                <th>Grand Total</th>
+                                                <th><input type="text" class="form-control form-control-sm"></th>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </br>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,23 +4,23 @@
     <meta name="description" content="A responsive bootstrap 4 admin dashboard template by hencework" />
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="favicon.ico">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-	
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <link rel="icon" href="{{ asset('dist/img/logo_dwi2.png') }}" type="image/x-icon">
+
 	<!-- vector map CSS -->
     <link href="{{ asset('vendors/vectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" type="text/css" />
 
     <!-- Toggles CSS -->
     <link href="{{ asset('vendors/jquery-toggles/css/toggles.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('vendors/jquery-toggles/css/themes/toggles-light.css') }}" rel="stylesheet" type="text/css">
-	
+
     <!-- Custom CSS -->
     <link href="{{ asset('dist/css/style.css') }}" rel="stylesheet" type="text/css">
 
     <!-- Data Table CSS -->
     <link href="{{ asset('vendors/datatables.net-dt/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('vendors/datatables.net-responsive-dt/css/responsive.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
-    
+
     <!-- select2 CSS -->
     <link href="{{ asset('vendors/select2/dist/css/select2.min.css') }}" rel="stylesheet" type="text/css" />
 
